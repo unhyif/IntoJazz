3edca9be8b3d49e4cfcaee478bae6ae34b91a5bc
Jihyun Kim <ang21icjh@gmail.com>
2022-06-23 21:43:59 +0900

    design: simplify Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,16 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
 import Body from 'layouts/Body/Body';
 import Footer from './Footer/Footer';
-import styles from './Layout.module.scss';
 
 const Layout = () => (
-  <div className={styles.layout}>
+  <>
     <Header />
     <Body>
       <Outlet />
     </Body>
     <Footer />
-  </div>
+  </>
 );
 
 export default Layout;
